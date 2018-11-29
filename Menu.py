a9c5fccf4d59c16c8e73cd4f19d1645046c7f1cc
helloAT <43610421+helloAT@users.noreply.github.com>
2018-11-29 09:18:53 -0500

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -1,11 +1,11 @@
-def menu(level):
-    if level == 0:
-        textSize(150)
-        textAlign(CENTER)
-        text("Golf", width / 2, height / 4)
-        rectMode(CENTER)
-        rect(width / 2, height / 2, 150, 75)
-        textSize(50)
-        fill(0)
-        text("Play!", width / 2, height / 2 + 15)
-        fill(255)
+def menu():
+    background(0, 255, 0)
+    textSize(150)
+    textAlign(CENTER)
+    text("Golf", width / 2, height / 4)
+    rectMode(CENTER)
+    rect(width / 2, height / 2, 150, 75)
+    textSize(50)
+    fill(0)
+    text("Play!", width / 2, height / 2 + 15)
+    fill(255)
